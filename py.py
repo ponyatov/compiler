43decc3776f88133e8dcac20bdd42c2214bbc876
Dmitry Ponyatov <dponyatov@gmail.com>
2017-02-01 15:03:43 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py.py
+++ py.py
@@ -8,7 +8,7 @@ c = -3e+04
 import ply.lex  as lex
 import ply.yacc as yacc
 
-tokens = [ 'SYM' , 'NUM' , 'OP' ]
+tokens = [ 'SYM' , 'NUM' , 'OP' , 'ADD','SUB' ]
 
 t_ignore_comment = r'\#.*'
 t_ignore = ' \t\r'
@@ -22,9 +22,19 @@ def t_NUM(t):
 def t_SYM(t):
     r'[a-zA-Z0-9_]+'
     return t
-def t_OP(t):
-    r'[\=\+\-\*\/\^]'
+def t_ADD(t):
+    r'\+'
     return t
+def t_SUB(t):
+    r'\-'
+    return t
+def t_OP(t):
+    r'[\=\*\/\^]'
+    return t
+
+precedence = [
+    ('right','PFX'),
+    ]
 
 def p_REPL_none(p):
     ' REPL : '
@@ -36,9 +46,15 @@ def p_ex_scalar(p):
             | NUM
             | OP    '''
     p[0] = p[1]
+def p_ex_pfx_add(p):
+    ' ex  : ADD ex %prec PFX '
+    p[0] = p[2]
+def p_ex_pfx_sub(p):
+    ' ex  : SUB ex %prec PFX '
+    p[0] = -p[2]
     
 def t_error(t): print 'error/lexer', t
 def p_error(p): print 'parse/lexer', p
 
-lexer = lex.lex()
+lex.lex()
 yacc.yacc(debug=False, write_tables=False).parse(SRC)
